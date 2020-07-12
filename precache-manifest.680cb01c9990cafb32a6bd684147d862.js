ca5fc80af6918a06364db05c810a49e452600c79
Eric <eric.vasquez3327@gmail.com>
2020-07-12 04:38:01 -0500

    Updates

---
+++ precache-manifest.680cb01c9990cafb32a6bd684147d862.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d3db1fe164792e5855b074392b9e61",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "151bb1d9e93d27d6f8a7",
+    "url": "/portfolio/static/css/main.7f324d9b.chunk.css"
+  },
+  {
+    "revision": "70de8f9179a9e3476a72",
+    "url": "/portfolio/static/js/2.ad561b17.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/portfolio/static/js/2.ad561b17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151bb1d9e93d27d6f8a7",
+    "url": "/portfolio/static/js/main.506b09ed.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
